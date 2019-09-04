676b911b4858ab83752ed6ed663e4b1f542e8d7c
Bryan Szendel <bryan.szendel@gmail.com>
2019-09-04 10:51:08 -0600

    added server and setup nodemon dev script

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // index file
+
+const server = require('./server.js')
+
+//manage products (8) 
+//manage clients (8)
+//manage orders 
+//manage suppliers 
+
+const port = 7000;
+server.listen(port, () => console.log(`\n** API on port ${port} **\n`))
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const server = express()
+
+
+
+
+server.get('/products', (req, res) => {
+  
+});
+
+server.get('/clients', (req, res) => {
+  
+});
+
+server.get('/suppliers', (req, res) => {
+  
+});
+
+module.exports = server;
